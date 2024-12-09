c40c62af7b7c76f818d6dfb9c0ed973923c14b26
Tommy Faizal <f_tommy@live.com>
2024-12-10 00:04:43 +1100

    add player event queue

--- src/queue/eventQueue.ts
+++ src/queue/eventQueue.ts
@@ -4,3 +4,7 @@ import { configuration } from "../config/config";
 export const eventQueue = new Queue("game-events", {
   connection: { host: configuration.redisHost, port: 6379 },
 });
+
+export const playerQueue = new Queue("player-events", {
+  connection: { host: configuration.redisHost, port: 6379 },
+});
