650bab99bcd965d73bcb69c3fcd4d00b1ed7f7c9
Tommy Faizal <f_tommy@live.com>
2024-12-10 00:07:04 +1100

    update test to for player event

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -2,15 +2,28 @@ import request from "supertest";
 import app from "../app";
 
 describe("POST /events", () => {
-  it("queues an event successfully", async () => {
+  it("queues a GAME_EVENT event successfully", async () => {
     const response = await request(app)
       .post("/events")
       .send({
-        type: "game-start",
+        type: "GAME_EVENT",
         data: { playerId: "JohnDoe" },
       });
     expect(response.status).toBe(200);
-    expect(response.body.message).toBe("Event queued successfully");
+    expect(response.body.message).toBe("Event GAME_EVENT queued successfully");
+  });
+
+  it("queues a PLAYER_EVENT event successfully", async () => {
+    const response = await request(app)
+      .post("/events")
+      .send({
+        type: "PLAYER_EVENT",
+        data: { name: "JohnDoe", score: 10, gamesPlayed: 1 },
+      });
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe(
+      "Event PLAYER_EVENT queued successfully"
+    );
   });
 
   it("returns 400 for invalid input", async () => {
